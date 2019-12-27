32561658b36c7c12cee94f43b5baee58eb2ab953
debika sarkar <debikasarkar713@gmail.com>
2019-12-27 10:41:34 -0500

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,16 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace :admin do 
+  
+  resources :blog_posts
+  get 'admins/index'
+  root to: 'admins#index'
+  end
+
+  get 'home/index'
+  
+  resources :blog_posts 
+
+  root 'home#index'
+end
