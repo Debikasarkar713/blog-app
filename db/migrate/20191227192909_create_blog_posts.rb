b3b2a70c0a0b9880d7540d236515f28a93731936
debika sarkar <debikasarkar713@gmail.com>
2019-12-27 15:00:07 -0500

    add schema

--- db/migrate/20191227192909_create_blog_posts.rb
+++ db/migrate/20191227192909_create_blog_posts.rb
@@ -2,7 +2,11 @@ class CreateBlogPosts < ActiveRecord::Migration[5.1]
   def change
     create_table :blog_posts do |t|
       t.string :title
-
+      t.string :author
+      t.text :content
+      t.text :excerpt
+      t.boolean :published, default: false
+      t.datetime :published_at
       t.timestamps
     end
   end
