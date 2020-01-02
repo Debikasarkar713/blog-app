c4fe606f354ca8d90a880d50474e09ff48cfce4e
debika sarkar <debikasarkar713@gmail.com>
2020-01-02 16:58:53 -0500

    Add styling

--- db/migrate/20200102045338_create_authors.rb
+++ db/migrate/20200102045338_create_authors.rb
@@ -3,6 +3,7 @@ class CreateAuthors < ActiveRecord::Migration[5.1]
     create_table :authors do |t|
       t.string :first_name
       t.string :last_name
+      t.references :blog_post, null: false, foreign_key: true
       t.timestamps
     end
   end
